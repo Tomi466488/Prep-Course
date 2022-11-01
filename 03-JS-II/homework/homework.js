3b3b77a57c812f4df1b5f1b2d0d87d2d60ae7191
unknown <tomi_877@hotmail.com>
2022-11-01 11:26:40 -0300

    Noveno ejercicio agregado del JS3

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -170,13 +170,22 @@ function esVerdadero(valor){
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
+  //Escribe tu código aquí 
+  let arrayTablaDe16 = []
+  for (let i = 0; i < 11; i++) {
+    arrayTablaDe16.push(6 * i)
+  }  
+  return arrayTablaDe16
   
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if (numero > 99 && numero < 1000) {
+    return true
+  }
+  return false
   
 }
 
@@ -184,6 +193,14 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero;
+  var i = 0;
+  do {
+    i = i + 1;
+    a = a + 5;
+  }
+  while (i < 8);
+  return a;
 }
 
 
