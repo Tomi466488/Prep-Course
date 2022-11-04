ca22c99045f1f07a2e7d766211709c375e9cac69
unknown <tomi_877@hotmail.com>
2022-11-04 17:37:00 -0300

    Ejercicio agregado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -23,6 +23,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let objeto = {}
+  for(let i = 0; i<string.length; i++){
+    if (object.keys(objeto).includes(string[i])){
+      objeto[string[i]] = objeto[string[i]] + 1
+      continue
+    }
+    objeto [string[i]] = 1
+  return objeto 
+  }
 }
 
 
