90bf5fa70a0edeb451f4f7c42f5756a4e20c660e
unknown <tomi_877@hotmail.com>
2022-11-07 17:41:22 -0300

    Ejercicio agregado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -101,7 +101,10 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  return numero % 1 === 0;
+  if (numero % 1 === 0){
+    return true;
+  }
+  return false;
 }
 
 function fizzBuzz(numero) {
@@ -109,12 +112,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 3 === 0)
-    return "fizz";
-  if (numero % 5 === 0)
-    return "buzz";
-  if (numero % 15 === 0)
-    return "fizzbuzz";
+  if (numero % 15 === 0){
+    return "fizzbuzz"
+  }
+  if (numero % 3 === 0){
+    return "fizz"
+  }
+  if (numero % 5 === 0){
+    return "buzz"
+  }
   return numero;
 }
 
@@ -125,17 +131,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num1 > num3 && num1 > 0){
-    return "Numero 1 es mayor y positivo";
-  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
-    return "hay negativos";
-  }else if (num3 > num1 && num3 > num2) {
-    return num3 + 1;
-  } else if (num1 === 0 || num2 === 0 || num3 === 0) {
-    return "Error";
-  } else {
-    return "False";
+  if (num1 < 0 || num2 < 0 || num3 < 0){
+    return "Hay negativos"
   }
+  else if (num1 === 0 || num2 === 0 || num3 === 0){
+    return "Error"
+  }
+  else if (num1 > 0 && num1 > num2 && num1 > num3){
+    return "Número 1 es mayor y positivo"  
+  }
+  else if (num3 > num1 && num3 > num2){
+    return num3 + 1;
+  }
+  return false;
 }
 
 function esPrimo(numero) {
